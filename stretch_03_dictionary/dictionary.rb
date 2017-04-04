2d3492cb74da5797fb3872da6cc23dd9de27e76b
quantumduck <quantum.duck@hotmail.com>
2017-04-04 18:53:22 -0400

    adding terms

--- stretch_03_dictionary/dictionary.rb
+++ stretch_03_dictionary/dictionary.rb
@@ -1,23 +1,30 @@
 class Dictionary < Hash
 
   def add(term)
-
+    case term.class
+    when String
+      store(term, nil)
+    when Hash
+      self[term.keys.first] = term.values.first
+    end
   end
 
-  def include?(term)
-
-  end
+  # def include?(term)
+  #
+  # end
 
   def entries
-
+    self
   end
 
   def keywords
-
+    keys
   end
 
   def find(term)
-
+    result = {}
+    keys.each { |k| result.store(k, self[k]) if k.start_with?(term) }
+    result
   end
 
 end
