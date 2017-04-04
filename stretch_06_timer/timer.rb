57760b7e42ced4fc517e231c5ea500d8c8eba338
quantumduck <quantum.duck@hotmail.com>
2017-04-04 19:34:24 -0400

    timer working

--- stretch_06_timer/timer.rb
+++ stretch_06_timer/timer.rb
@@ -1,15 +1,32 @@
 class Timer
 
-  def seconds
+  attr_accessor :seconds
 
-  end
-
-  def seconds=(num)
-
+  def initialize
+    @seconds = 0
   end
 
   def time_string
-
+    secs = @seconds
+    result = ""
+    hours = secs / 3600
+    result += pad(hours)
+    result += ':'
+    secs -= 3600 * hours
+    minutes = secs / 60
+    result += pad(minutes)
+    result += ':'
+    secs -= 60 * minutes
+    result += pad(secs)
   end
 
+  private
+
+  def pad(num)
+    if (num <= 9)
+      result = '0' + num.to_s
+    else
+      result = num.to_s
+    end
+  end
 end
