62966aa9f570a69a4f3a3b7316aafb213e919030
quantumduck <quantum.duck@hotmail.com>
2017-04-04 19:12:22 -0400

    silly blocks done

--- stretch_04_silly_blocks/silly_blocks.rb
+++ stretch_04_silly_blocks/silly_blocks.rb
@@ -1,11 +1,18 @@
 def reverser
-
+  words = yield.split
+  result = words.first.reverse
+  while words.length > 1
+    result += ' '
+    words.delete_at(0)
+    result += words.first.reverse
+  end
+  result
 end
 
-def repeater(n = 2)
-
+def repeater(n = 1)
+  n.times { yield }
 end
 
 def adder(n = 1)
-
+  yield + n
 end
