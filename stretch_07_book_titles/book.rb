208cebf7b765d1d5edac396d963e18ea2c0d61e4
quantumduck <quantum.duck@hotmail.com>
2017-04-04 20:00:34 -0400

    fixed %w syntax error

--- stretch_07_book_titles/book.rb
+++ stretch_07_book_titles/book.rb
@@ -1,12 +1,12 @@
 class Book
 
-  @@short_words = %w(and, but, a, an, the, in, of, on, with, over, under, above, for, at)
+  @@short_words = %w(and but a an the in of on with over under above for at)
 
   attr_reader :title
 
   def title=(title)
-    titlewords = title.split
-    titlewords.each { |word| word.capitalize! if !@@short_words.include?(word) }
+    titlewords = title.downcase.split
+    titlewords.each { |word| word.capitalize! if !@@short_words.include?(word.to_s) }
     titlewords.first.capitalize!
     @title = titlewords.join(' ')
   end
